38c0f62bb19040cbfb4501abc694668725674e94
Nils Carlier <nils.c56@gmail.com>
2026-08-02 09:37:47 +0200

    fix CI OVMF firmware discovery

--- crates/logos-test/src/main.rs
+++ crates/logos-test/src/main.rs
@@ -1540,6 +1540,7 @@ fn ovmf_path() -> Option<String> {
     env::var("OVMF_CODE").ok().filter(|path| Path::new(path).is_file()).or_else(|| {
         [
             "C:/Program Files/qemu/share/edk2-x86_64-code.fd",
+            "/usr/share/OVMF/OVMF_CODE_4M.fd",
             "/usr/share/OVMF/OVMF_CODE.fd",
             "/usr/share/edk2/x64/OVMF_CODE.fd",
         ]
