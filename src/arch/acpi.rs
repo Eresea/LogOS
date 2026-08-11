96d3a1b8b16015c23eef71f9539068d5c2208639
Nils Carlier <nils.c56@gmail.com>
2026-08-11 19:22:13 +0200

    test: cover duplicate ACPI processors

--- src/arch/acpi.rs
+++ src/arch/acpi.rs
@@ -585,9 +585,19 @@ mod tests {
     }
 
     #[test]
-    fn rejects_malformed_processor_entries() {
+    fn rejects_malformed_and_duplicate_processor_entries() {
         assert_eq!(CpuTopology::parse(&[0, 7, 0, 2, 1, 0, 0], None), Err(TopologyError::Malformed));
         assert_eq!(CpuTopology::parse(&[9, 15, 0, 0], None), Err(TopologyError::Malformed));
+        assert_eq!(
+            CpuTopology::parse(
+                &[
+                    0, 8, 0, 2, 1, 0, 0, 0,
+                    0, 8, 0, 2, 1, 0, 0, 0,
+                ],
+                None,
+            ),
+            Err(TopologyError::Malformed)
+        );
     }
 
     #[test]
