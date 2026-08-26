03f440a95d221070b6881fbb26a1e6d18b0acdaf
Nils Carlier <nils.c56@gmail.com>
2026-08-26 19:42:12 +0200

    fix(qemu): show interactive framebuffer

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -82,13 +82,14 @@ if ($networkEnabled) {
 }
 
 $espPath = ((Resolve-Path $esp).Path).Replace('\', '/')
-$display = if ($interactiveMode) { 'gtk' } else { 'none' }
+$display = if ($interactiveMode) { 'gtk,zoom-to-fit=on' } else { 'none' }
 $qemuArgs = @(
     '-machine', 'q35', '-m', '256M', '-smp', $Cpus,
     '-drive', "if=pflash,format=raw,readonly=on,file=$ovmf",
     '-drive', "format=raw,file=fat:rw:$espPath",
     '-drive', "if=none,id=storage-disk,format=raw,file=$disk,cache=writethrough",
     '-device', 'virtio-blk-pci,drive=storage-disk,disable-legacy=on',
+    '-vga', 'std',
     '-display', $display
 )
 if ($networkEnabled) {
